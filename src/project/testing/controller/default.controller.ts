f4f1602c920bdcb4c7b962c73d6951d6400f293d
Alexandre-RICHARD <alexandre.richard.dev@gmail.com>
2025-09-29 23:07:52 +0200

    reset to default config

--- src/project/testing/controller/default.controller.ts
+++ src/project/testing/controller/default.controller.ts
@@ -1,30 +1,12 @@
-import type { testSpecs } from "@specs/testing/specs/test.specs.ts";
-
-import type { AsyncControllerByAlias } from "../../../common/AsyncControllerByAlias.type.ts";
-import { toTestDtoMapper } from "../dto/mapper/toTestDto.mapper.ts";
-import { toTestsDtoMapper } from "../dto/mapper/toTestsDto.mapper.ts";
-import { getAllTests } from "../query/getAllTests.ts";
-import { getOneTest } from "../query/getOneTest.ts";
-
-type ControllerCustom = {
-	getAllTests: AsyncControllerByAlias<typeof testSpecs, "get", "/test/all">;
-	getOneTest: AsyncControllerByAlias<typeof testSpecs, "get", "/test/one">;
-};
+import { asyncRequestHandler } from "../../../common/asyncRequestHandler.ts";
 
 export const testController = {
-	getAllTests: async (_req, res) => {
-		const result = await getAllTests();
-		res.status(200).json(toTestsDtoMapper(result));
-	},
-	getOneTest: async (_req, res) => {
-		const result = await getOneTest();
-		if (!result) {
-			res.status(500).json({
-				code: 500,
-				message: "Fail",
-			});
-		} else {
-			res.status(200).json(toTestDtoMapper(result));
-		}
-	},
-} as ControllerCustom;
+	test: asyncRequestHandler((req, res) => {
+		console.log({
+			params: req.params,
+			query: req.query,
+			body: req.body,
+		});
+		res.json({ test: "réussi" });
+	}),
+};
